6425e6e4d95b4eaa5688d23ac86cddcdde15f9b0
Grégoire Alexandre <210278@CCM-Asty-Moulin.local>
2023-12-01 16:12:10 +0100

    quelque truc sur index 2

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -7,3 +7,7 @@ get('/', function () {
 get('/test1', function () {
     include_once __DIR__ . "/../Views/test1.php";
 });
+
+get('/test2', function () {
+    include_once __DIR__ . "/../Views/test2.php";
+});
---
+++ Views/test2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/assets/styles/index.css">
+    <title>ez</title>
+</head>
+<body>
+    <header>
+        <ul>
+            <li> <a href="/">Home</a></li>
+            <li> <a href="">Créer une ecole</a></li>
+            <li> <a href="">Connexion</a></li>
+        </ul>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
