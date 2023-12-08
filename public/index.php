0d1b8125c1930b4ef11f5f330359cd263c9eb15e
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2023-12-08 14:22:39 +0100

    feat: add database

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ session_start();
 require_once __DIR__ . "/../Config/dotenv.php";
 (new DotEnv(__DIR__ . "/../.env"))->load();
 
+require_once __DIR__ . "/../Config/database.php";
+
 include_once __DIR__ . "/../router.php";
 
 include_once __DIR__ . "/../Controllers/HomeController.php";
